4716250a673301250e9346b77e8df502d6983e12
markriley9999 <mark.riley@digitaluk.co.uk>
2019-02-07 15:29:05 +0000

    Trying to fix av muxing

--- strippedSTVCentral/stripstream.sh
+++ strippedSTVCentral/stripstream.sh
@@ -95,8 +95,8 @@ if [ "$1" == "--fixav" ]; then
     ffmpeg -y -i tmp/av3-stvplus1.mp4 -vf "scale=-1:480, crop=720:480" -c:v libx264 -b:v 823k -maxrate:0 823k -minrate:0 823k -bufsize:0 1000000 -x264opts keyint=12:min-keyint=12:no-scenecut -c:a aac -ac 2 -ab 128000 -ar 48000 -b:a 128k -maxrate:1 128k -minrate:1 128k -bufsize:1 200000 -muxrate 1200000 -fflags +genpts -f mpegts -mpegts_original_network_id 1 -mpegts_transport_stream_id 1 -mpegts_service_id 8373 -mpegts_pmt_start_pid 1900 -streamid 0:1901 -streamid 1:1902 -metadata service_provider="CHANGEME" -metadata service_name="CHANGEME" -map 0:0 -map 0:1 tmp/spts-stvplus1.ts 
 
 
-    tsmask tmp/spts-stv.ts -8191 -0 -16 -17 > tmp/spts-stv-nosi.ts
-    tsmask tmp/spts-stvplus1.ts -8191 -0 -16 -17 > tmp/spts-stvplus1-nosi.ts
+    tsmask tmp/spts-stv.ts -0 -16 -17 > tmp/spts-stv-nosi.ts
+    tsmask tmp/spts-stvplus1.ts -0 -16 -17 > tmp/spts-stvplus1-nosi.ts
 
 
 
@@ -105,7 +105,7 @@ if [ "$1" == "--fixav" ]; then
 
 
 
-    tscbrmuxer c:12000000 tmp/stripped-noav.ts b:1200000 tmp/spts-stv-nosi.ts b:1200000 tmp/spts-stvplus1-nosi.ts > output/strippedSTVCentral-newAV.ts
+    tscbrmuxer c:1200000 tmp/spts-stv-nosi.ts b:1200000 tmp/spts-stvplus1-nosi.ts b:12000000 tmp/stripped-noav.ts > output/strippedSTVCentral-newAV.ts
 
 fi
 
