bcd91c04bed3862d4555d5fdc1355658255eb544
markriley9999 <mark.riley@digitaluk.co.uk>
2019-04-15 17:06:57 +0100

    spanner

--- makeAppStream/makeAppByIPStream.sh
+++ makeAppStream/makeAppByIPStream.sh
@@ -9,9 +9,9 @@ ffmpeg -y -i ~/source/keep-CosmosLaundromat.mp4 -c copy -ss 00:06:30 -t 90 tmp/a
 
 ffmpeg -y -i tmp/av.mp4  -vf "scale=-1:480, crop=720:480" -c:v libx264 -x264opts keyint=12:min-keyint=12:no-scenecut -c:a aac -ac 2 tmp/av2.mp4
 
-ffmpeg -y -i tmp/av2.mp4 -i ~/source/STV_logo_2014_scale.png -filter_complex "overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -codec:a copy tmp/av3.mp4
+ffmpeg -y -i tmp/av2.mp4 -i spanner.png -filter_complex "overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -codec:a copy tmp/av3.mp4
 
-ffmpeg -y -i tmp/av3.mp4 -vf "scale=-1:480, crop=720:480" -c:v libx264 -b:v 823k -maxrate:0 823k -minrate:0 823k -bufsize:0 1000000 -x264opts keyint=12:min-keyint=12:no-scenecut -c:a aac -ac 2 -ab 128000 -ar 48000 -b:a 128k -maxrate:1 128k -minrate:1 128k -bufsize:1 200000 -muxrate 1200000 -fflags +genpts -f mpegts -mpegts_original_network_id 1 -mpegts_transport_stream_id 1 -mpegts_service_id 4000 -mpegts_pmt_start_pid 3399 -streamid 0:3402 -streamid 1:3403 -metadata service_provider="CHANGEME" -metadata service_name="CHANGEME" -map 0:0 -map 0:1 tmp/spts.ts 
+ffmpeg -y -i tmp/av3.mp4 -vf "scale=-1:480, crop=720:480" -c:v libx264 -b:v 823k -maxrate:0 823k -minrate:0 823k -bufsize:0 1000000 -x264opts keyint=12:min-keyint=12:no-scenecut -c:a aac -ac 2 -ab 128000 -ar 48000 -b:a 128k -maxrate:1 128k -minrate:1 128k -bufsize:1 200000 -muxrate 1200000 -fflags +genpts -f mpegts -mpegts_original_network_id 1 -mpegts_transport_stream_id 1 -mpegts_service_id 4000 -mpegts_pmt_start_pid 3400 -streamid 0:3402 -streamid 1:3403 -metadata service_provider="CHANGEME" -metadata service_name="CHANGEME" -map 0:0 -map 0:1 tmp/spts.ts 
 
 tsmask tmp/spts.ts -0 -16 -17 -3400 > tmp/spts-nosi.ts
 
